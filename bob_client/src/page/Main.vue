cc17e4d02bfd8d66594514ef932e07c339c1f5ea
chu9741 <95557557+chu9741@users.noreply.github.com>
2023-08-27 16:52:13 +0900

    [Feature] NavBar 수정 (#12)
    * [Feat] NavBar 위치변경
    
    * [Feat] NavBar의 아이콘 Path변경
    
    * [Feat] NavBar 배경정리

--- bob_client/src/page/Main.vue
+++ bob_client/src/page/Main.vue
@@ -1,140 +1,139 @@
 <template>
-    <div class="allpage">
-        <div class="common-layout">
-            <el-container class="layout-container">
-                <el-header class="layout-header">
-                    Header <Menu></Menu></el-header>
-                <el-container class="layout-container">
-                    <!-- <el-aside width="200px" class="layout-aside">Aside -->
-                    <!-- <SmallMenu> </SmallMenu> -->
-                    <!-- </el-aside> -->
+  <!-- <el-header class="layout-header"> Header <Menu></Menu></el-header> -->
+  <Menu />
+  <div class="allpage">
+    <div class="common-layout">
+      <el-container class="layout-container">
+        <el-container class="layout-container">
+          <!-- <el-aside width="200px" class="layout-aside">Aside -->
+          <!-- <SmallMenu> </SmallMenu> -->
+          <!-- </el-aside> -->
 
-                    <el-container class="layout-container">
-                        <el-main class="layout-main">
-                            <router-view />
-                        </el-main>
-                    </el-container>
-                </el-container>
-                <div>
-                    <el-footer class="layout-footer">Footer</el-footer>
-                </div>
-            </el-container>
-
-            <button @click="openModal">모달 열기</button>
-            <Modal :visible="isModalOpen" @close="closeModal" :step="step"></Modal>
-            <button @click="openRateModal">평점 열기</button>
-            <RateModal :visible="isRateModalOpen" @close="closeRateModal"></RateModal>
-
-            <!-- <button id="btn" onclick="changeBtnName()">클릭!!</button> -->
+          <el-container class="layout-container">
+            <el-main class="layout-main">
+              <router-view />
+            </el-main>
+          </el-container>
+        </el-container>
+        <div>
+          <el-footer class="layout-footer">Footer</el-footer>
         </div>
+      </el-container>
+
+      <button @click="openModal">모달 열기</button>
+      <Modal :visible="isModalOpen" @close="closeModal" :step="step"></Modal>
+      <button @click="openRateModal">평점 열기</button>
+      <RateModal :visible="isRateModalOpen" @close="closeRateModal"></RateModal>
+
+      <!-- <button id="btn" onclick="changeBtnName()">클릭!!</button> -->
     </div>
+  </div>
 </template>
-  
+
 <script>
-import { ref } from 'vue';
-import Menu from '@/page/Menu.vue';
-import Modal from '@/page/Modal.vue';
-import RateModal from '@/page/RateModal.vue';
-import users from '@/typeuser.js';
+import { ref } from "vue";
+import Menu from "@/page/Menu.vue";
+import Modal from "@/page/Modal.vue";
+import RateModal from "@/page/RateModal.vue";
+import users from "@/typeuser.js";
 
 export default {
-    name: 'Main',
+  name: "Main",
 
-    data() {
-        return {
-            value: null,
-            step: 3,
-            유저들: users,
-            유저들원본: [...users],
-            누른거: 0,
-        };
-    },
-    components: {
-        Menu,
-        Modal,
-        RateModal,
-    },
-    setup() {
-        const activeIndex = ref('1');
-        const activeIndex2 = ref('1');
+  data() {
+    return {
+      value: null,
+      step: 3,
+      유저들: users,
+      유저들원본: [...users],
+      누른거: 0,
+    };
+  },
+  components: {
+    Menu,
+    Modal,
+    RateModal,
+  },
+  setup() {
+    const activeIndex = ref("1");
+    const activeIndex2 = ref("1");
 
-        // Variables for modals
-        const isModalOpen = ref(false);
-        const isRateModalOpen = ref(false);
+    // Variables for modals
+    const isModalOpen = ref(false);
+    const isRateModalOpen = ref(false);
 
-        const handleSelect = (key, keyPath) => {
-            console.log(key, keyPath);
-        };
+    const handleSelect = (key, keyPath) => {
+      console.log(key, keyPath);
+    };
 
-        const openModal = () => {
-            isModalOpen.value = true;
-        };
+    const openModal = () => {
+      isModalOpen.value = true;
+    };
 
-        const closeModal = () => {
-            isModalOpen.value = false;
-        };
+    const closeModal = () => {
+      isModalOpen.value = false;
+    };
 
-        const openRateModal = () => {
-            isRateModalOpen.value = true;
-        };
+    const openRateModal = () => {
+      isRateModalOpen.value = true;
+    };
 
-        const closeRateModal = () => {
-            isRateModalOpen.value = false;
-        };
+    const closeRateModal = () => {
+      isRateModalOpen.value = false;
+    };
 
-        return {
-            activeIndex,
-            activeIndex2,
-            handleSelect,
-            openModal,
-            closeModal,
-            openRateModal,
-            closeRateModal,
-            isModalOpen,
-            isRateModalOpen,
-        };
-    },
+    return {
+      activeIndex,
+      activeIndex2,
+      handleSelect,
+      openModal,
+      closeModal,
+      openRateModal,
+      closeRateModal,
+      isModalOpen,
+      isRateModalOpen,
+    };
+  },
 };
 </script>
-  
+
 <style>
 .list-box {
-    background-color: rgb(255, 255, 255);
+  background-color: rgb(255, 255, 255);
 }
 
 .layout-container {
-    background-color: #f0f0f0;
-    border: 1px solid #ccc;
-    margin: 10px;
-    padding: 10px;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  margin: 10px;
+  padding: 10px;
 }
 
 .layout-header {
-    background-color: #2196f3;
-    color: white;
-    padding: 10px;
+  background-color: #2196f3;
+  color: white;
 }
 
 .layout-aside {
-    background-color: #4caf50;
-    color: white;
-    padding: 10px;
+  background-color: #4caf50;
+  color: white;
+  padding: 10px;
 }
 
 .layout-main {
-    background-color: rgb(255, 255, 128);
-    padding: 10px;
+  background-color: rgb(255, 255, 128);
+  padding: 10px;
 }
 
 .layout-footer {
-    background-color: #f44336;
-    color: white;
-    padding: 10px;
-    text-align: center;
+  background-color: #f44336;
+  color: white;
+  padding: 10px;
+  text-align: center;
 }
 
 .allpage {
-    width: 70%;
-    margin: auto;
+  width: 70%;
+  margin: auto;
 }
 </style>
--- bob_client/src/page/Menu.vue
+++ bob_client/src/page/Menu.vue
@@ -1,23 +1,45 @@
 <template>
-  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-    <router-link to="/"><el-menu-item> <img class="logo" src="@/assets/image.jpg"> </el-menu-item></router-link>
-    <router-link to="/list"><el-menu-item index="1">룸메이트 찾기</el-menu-item></router-link>
-    <router-link to="/findroom"><el-menu-item index="2">부동산 찾기</el-menu-item></router-link>
-    <router-link to="/alarm"><el-menu-item index="3">알림</el-menu-item></router-link>
-    <router-link to="/mypage"><el-menu-item index="4">마이페이지</el-menu-item></router-link>
-  </el-menu>
+  <div class="elMenuDemo">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+    >
+      <router-link to="/home"
+        ><el-menu-item>
+          <img class="logo" src="@/assets/image.jpg" /> </el-menu-item
+      ></router-link>
+      <router-link to="/list"
+        ><el-menu-item index="1">룸메이트 찾기</el-menu-item></router-link
+      >
+      <router-link to="/findroom"
+        ><el-menu-item index="2">부동산 찾기</el-menu-item></router-link
+      >
+      <router-link to="/alarm"
+        ><el-menu-item index="3">알림</el-menu-item></router-link
+      >
+      <router-link to="/mypage"
+        ><el-menu-item index="4">마이페이지</el-menu-item></router-link
+      >
+    </el-menu>
+  </div>
   <div class="h-6" />
-  <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" background-color="#545c64"
-    text-color="#fff" active-text-color="#ffd04b" @select="handleSelect">
+  <el-menu
+    :default-active="activeIndex2"
+    class="el-menu-demo"
+    mode="horizontal"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+    @select="handleSelect"
+  >
   </el-menu>
 </template>
 
 <script>
 export default {
   name: "Menu",
-  data() {
-    return {};
-  },
 };
 </script>
 
@@ -25,6 +47,10 @@ export default {
 .logo {
   width: 50px;
   height: 50px;
+}
 
+.elMenuDemo {
+  background-color: "blanchedalmond";
+  /* border-radius: 30px; */
 }
 </style>
