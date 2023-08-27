a63fda830d1f2cf1496fc6e84af43ddd0164a1d0
chu9741 <chu9741@gmail.com>
2023-08-27 19:00:17 +0900

    [Feat] 룸메이트 찾기 - 유저정보 출력

--- bob_client/src/page/List.vue
+++ bob_client/src/page/List.vue
@@ -1,7 +1,17 @@
 <template>
   <h3>룸메이트 찾기 페이지</h3>
-  <el-card v-for="nominee in nominees" :key="nominee.id">
-    <div>{{ nominee.userName }}</div>
+  <el-card
+    v-for="nominee in nominees"
+    :key="nominee.id"
+    style="margin-bottom: 4px"
+  >
+    <el-avatar :style="spanStyle" :src="nominee.userPhoto" />
+    <span :style="spanStyle">{{ nominee.userName }}</span>
+    <span :style="spanStyle">{{ nominee.userMBTI }}</span>
+    <span :style="spanStyle">{{ nominee.userBirthYear }}</span>
+    <span :style="spanStyle">
+      <el-button type="warning" style="float: right">신청</el-button>
+    </span>
     <!-- 다른 속성들도 필요에 따라 출력할 수 있습니다 -->
   </el-card>
 </template>
@@ -24,13 +34,14 @@ export default {
 
     const getAllRoommateNominees = async () => {
       try {
-        console.log(localStorage.getItem("JWT"));
+        console.log(JSON.parse(localStorage.getItem("JWT")).data);
 
         const response = await axios.get("api/users/search", {
           headers: {
-            token: localStorage.getItem("JWT"),
+            token: encodeURIComponent(localStorage.getItem("JWT")),
           },
         });
+        console.log(response.data);
         nominees.value = response.data; // 가져온 데이터를 nominees에 할당
       } catch (error) {
         console.error("Error fetching nominees:", error);
@@ -43,6 +54,9 @@ export default {
 
     return {
       nominees,
+      spanStyle: {
+        marginRight: "1em",
+      },
     };
   },
 };
