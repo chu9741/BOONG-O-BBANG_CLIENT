c7495141efec57e8912294995eac9c583c4d13fb
leeyeonjoo123 <yjlee22501@gmail.com>
2023-08-28 18:07:29 +0900

    [Chore] 메뉴 밑줄제거, 안쓰는 vue제거

--- bob_client/src/page/Menu.vue
+++ bob_client/src/page/Menu.vue
@@ -6,20 +6,20 @@
       mode="horizontal"
       @select="handleSelect"
     >
-      <router-link to="/home"
+      <router-link to="/home" style="text-decoration: none;"
         ><el-menu-item>
           <img class="logo" src="@/assets/image.jpg" /> </el-menu-item
       ></router-link>
-      <router-link to="/list"
+      <router-link to="/list" style="text-decoration: none;"
         ><el-menu-item index="1">룸메이트 찾기</el-menu-item></router-link
       >
-      <router-link to="/findroom"
+      <router-link to="/findroom" style="text-decoration: none;"
         ><el-menu-item index="2">부동산 찾기</el-menu-item></router-link
       >
-      <router-link to="/alarm"
+      <router-link to="/alarm" style="text-decoration: none;"
         ><el-menu-item index="3">알림</el-menu-item></router-link
       >
-      <router-link to="/mypage"
+      <router-link to="/mypage" style="text-decoration: none;"
         ><el-menu-item index="4">마이페이지</el-menu-item></router-link
       >
     </el-menu>
@@ -33,6 +33,7 @@
     text-color="#fff"
     active-text-color="#ffd04b"
     @select="handleSelect"
+    
   >
   </el-menu>
 </template>
@@ -51,6 +52,7 @@ export default {
 
 .elMenuDemo {
   background-color: "blanchedalmond";
-  /* border-radius: 30px; */
 }
+
+
 </style>
